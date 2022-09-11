6056109d7a8dc0d98a311cf1e68957b67033b261
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2022-09-11 18:53:52 +0300

    Assignment finished.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,32 @@
-console.log('👻');
+import data from "../data.json";
+
+let formatedStudents = {};
+
+function getGroups() {
+	const students = data.filter((student) => student.type === null);
+	const assistant = data.filter((student) => student.type !== null);
+
+	assistant.map((assistant) => {
+		formatedStudents = {
+			...formatedStudents,
+			[assistant.group]: {
+				students: [],
+				assistant: assistant.name,
+			},
+		};
+
+		const filteredStudents = students.filter(
+			(student) => student.group === assistant.group
+		);
+
+		formatedStudents[assistant.group].students = filteredStudents;
+	});
+	console.log(formatedStudents);
+}
+
+function filterByGroupName(groupName) {
+	console.log(formatedStudents[groupName]);
+}
+
+getGroups();
+filterByGroupName("SteelBlue");
