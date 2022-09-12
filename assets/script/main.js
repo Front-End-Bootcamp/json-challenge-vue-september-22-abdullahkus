4c3aba7701ac38d24089dc52267843f3ef910d70
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2022-09-12 15:44:58 +0300

    Assignment Rezived.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,31 +1,40 @@
-import data from "../data.json";
+import data from "../data.json"; // data çekildi.
 
-let formatedStudents = {};
+let formatedStudents = {}; // Formatladığım objeye global erişim sağlamak için tanımlandı.
 
 function getGroups() {
-	const students = data.filter((student) => student.type === null);
-	const assistant = data.filter((student) => student.type !== null);
+	const students = data.filter((student) => student.type === null); // Gelen data'daki öğrencileristudents'e atandı.
+	const assistant = data.filter((student) => student.type !== null); // Gelen data'daki asistanlar assistant'a atandı.
 
 	assistant.map((assistant) => {
-		formatedStudents = {
-			...formatedStudents,
-			[assistant.group]: {
-				students: [],
-				assistant: assistant.name,
-			},
-		};
+		// Her grupda asistan için dizi gönderilmediğinden dolayı sadece tek asistan olabilir olarak anladım ve asistanları tek tek gezerek grup isimleri dinamik olarak elde etmek için map'lendi.
+		if (!formatedStudents[assistant.group]) {
+			// Grup daha önce oluşturuldu mu diye kontrol edildi.
+			formatedStudents = {
+				...formatedStudents, // Obje'nin şuanki halini aldı.
+				[assistant.group]: {
+					// Obje içerisine grup adında bir obje oluşturuldu.
+					students: [],
+					assistant: assistant.name, // asistan atandı.
+				},
+			};
+		}
 
+		// Öğrencilerin grubuyla eşleşenler filtelendi.
 		const filteredStudents = students.filter(
 			(student) => student.group === assistant.group
 		);
 
-		formatedStudents[assistant.group].students = filteredStudents;
+		// Filterelen öğrencilerin isimleri grubun öğrencilerine atandı.ßß
+		filteredStudents.map((student) => {
+			formatedStudents[assistant.group].students.push(student.name);
+		});
 	});
 	console.log(formatedStudents);
 }
 
 function filterByGroupName(groupName) {
-	console.log(formatedStudents[groupName]);
+	console.log(formatedStudents[groupName]); // Gönderilen grup ismini formatlanan öğrenci objesinde aranarak ilgili veriye erişildi.
 }
 
 getGroups();
